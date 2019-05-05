a13158f99cf5c64aff33d8f4d2fc8adaf6b07293
Oliver Veits <oliver.veits@web.de>
2019-05-05 15:10:33 +0200

    move ENV defaults to entrypoint

--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 [ "$SELENIUM_HUB_URL" == "" ] && export SELENIUM_HUB_URL="http://selenium_hub:4444/wd/hub"
+[ "$BROWSERS" == "" ] && export BROWSERS=chrome
+[ "$BROWSER" == "" ] && export BROWSER=chrome
+[ "$INGRESS_HOSTNAME" == "" ] && export INGRESS_HOSTNAME=crochunter.vocon-it.com
+
 
 echo "SELENIUM_HUB_URL=$SELENIUM_HUB_URL"
 
